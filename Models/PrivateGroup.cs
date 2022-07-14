d61b70bafacc1776bf283002f20588f6247e5dc4
Tkmtv <tin.nguyen.set18@eiu.edu.vn>
2022-07-14 15:41:17 +0700

    partial finish of group service
    partial finish of group service

--- Models/PrivateGroup.cs
+++ Models/PrivateGroup.cs
@@ -6,7 +6,7 @@ namespace ChatApp.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public IEnumerable<User> AdminList { get; set; }
+        public User GroupAdmin { get; set; }
         public IEnumerable<User> MemberList { get; set; }
     }
 }
--- Models/PublicGroup.cs
+++ Models/PublicGroup.cs
@@ -6,6 +6,7 @@ namespace ChatApp.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string InviteCode { get; set; }
         public IEnumerable<User> MemberList { get; set; }
     }
 }
